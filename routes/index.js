1efaf686604e6b00418d01e2aab9154c651b0c06
Saransh <saransh.ahlawat94@gmail.com>
2019-04-24 18:47:41 -0400

    added more routes and placeholder html

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const rootRoutes = require("./root")
 const loginRoutes = require("./login")
+const registerRoutes = require("./register")
+const homeRoutes = require("./home")
 // const postsRoutes = require("./posts")
 
 const constructorMethod = app => {
   app.use("/", rootRoutes);
   app.use("/login", loginRoutes);
-//   app.use("/login", postsRoutes);
-//   app.use("/sign-up", animalRoutes);
-//   app.use("/home", postsRoutes);
+  app.use("/register", registerRoutes);
+  app.use("/home", homeRoutes);
 
   app.use("*", (req, res) => {
     res.status(404).json({ error: "Not found" });
