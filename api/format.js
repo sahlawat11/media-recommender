365fd73f5a0187832a2ca732b69eea1fdd2867f4
jwestfromtheeast <justinwestley11@gmail.com>
2019-05-11 18:17:19 -0400

    Added Spotify callbacks, ability to lookup by id from a search

--- api/format.js
+++ api/format.js
@@ -5,33 +5,43 @@ const spotify = require("./spotify");
  * Example to use the functions in omdb.js
  */
 const omdbRunner = async () => {
-  const res = await omdb.getByName("House");
   // Grab the first show of the list
-  console.log(res.Search[0]);
   const id = await omdb.getByIdByName("House", 0);
   console.log(id);
 };
 
-/**
- * Example to use the Spotify functions in spotify.js
- * @param {function} res 
- */
-const trackCallback = async res => {
-  console.log(res.tracks.items[0]);
+const trackIdCallback = async res => {
+    console.log(res);
 };
 
 /**
  * Example to use the Spotify functions in spotify.js
- * @param {function} res 
+ * @param {function} res
+ */
+const trackCallback = async (res, callback) => {
+  const trackId = res.tracks.items[0].id;
+  console.log(trackId);
+  const trackInfo = await spotify.getByTrackId(trackId, trackIdCallback);
+};
+
+const artistIdCallback = async res => {
+    console.log(res);
+}
+
+/**
+ * Example to use the Spotify functions in spotify.js
+ * @param {function} res
  */
 const artistCallback = async res => {
-    console.log(res.artists.items[0])
-}
+  const artistId = res.artists.items[0].id;
+  console.log(artistId);
+  const artistInfo = await spotify.getByArtistId(artistId, artistIdCallback);
+};
 
 // Runner for the Spotify and IMDB APIs
 const main = async () => {
-  //const res = await spotify.searchByTrack("Nonstop", trackCallback);
-  //const spot = await spotify.searchByArtist("Drake", artistCallback);
+  const res = await spotify.searchByTrack("Nonstop", trackCallback);
+  const spot = await spotify.searchByArtist("Drake", artistCallback);
   //omdbRunner();
 };
 
