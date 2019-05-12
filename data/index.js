15555051697bf51816a20f7cdbfdbd0a6e424855
70Song <70Song@github.com>
2019-05-12 15:55:57 -0400

    commit message

--- data/index.js
+++ data/index.js
@@ -1,7 +1,9 @@
 const users = require("./users");
 const playlists = require("./playlists");
+const movies = require("./movies")
 
 module.exports={
     users,
-    playlists
+    playlists,
+    movies
 }
