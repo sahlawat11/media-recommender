45fb85939dcf6803aee76bacdc9e67e4037c7b0d
Saransh <saransh.ahlawat94@gmail.com>
2019-05-14 16:29:07 -0400

    changed html to pass html validator

--- routes/root.js
+++ routes/root.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const path = require("path");
 
 router.get("/", async (req, res) => {
+  if(req.session.loggedIn) {
+    res.redirect("/profile/my-profile");
+  } else {
   res.render("root");
+  }
 });
 
 router.get("/logout", async (req, res) => {
