8a75d0ac41f2a937e9629f5ee7f3bfe9dd2c715e
Tianxin-Liu <liutianxin1993@gmail.com>
2019-05-13 21:24:30 -0400

    fix style

--- movieInfo.js
+++ movieInfo.js
@@ -1,23 +1,5 @@
 module.exports = [
     {
-<<<<<<< HEAD
-        "MovieId": "b773698c",
-        "Name": "Nonstop",
-        "Artist": "Drake",
-        "Genres": "Comedy"
-    },
-    {
-        "MovieId": "b773698cd",
-        "Name": "Nonsto1p",
-        "Artist": "Drakl",
-        "Genres": "Comedy"
-    },
-    {
-        "MovieId": "b773698sc",
-        "Name": "Nonst3op",
-        "Artist": "Drakc",
-        "Genres": "Comedy"
-=======
         "MovieId": "tt2724064",
         "Name": "Sharknado",
         "Genres": ["Thriller", "Horror", "Comedy"]
@@ -141,6 +123,5 @@ module.exports = [
         "MovieId": "tt5463162",
         "Name": "Deadpool 2",
         "Genres": ["Action", "Adventure", "Comedy"]
->>>>>>> c6e109366c591c8b9a614bee4340878114d1b61e
     }
 ]
