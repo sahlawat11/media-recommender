07f0f548b6d35d71b230636d4502aabf0060e884
70Song <70Song@github.com>
2019-05-15 14:09:22 -0400

    commit status

--- seed.js
+++ seed.js
@@ -11,59 +11,27 @@ const main = async () => {
     const db = await dbConnection();
     await db.dropDatabase();
     
-    let listinfo1 ={
-        "Name": "to be calm",
-        "Type": "music",
-        "Owner": "Sunii Kim",
-        "Status": "public",
-        "Media": [{
-            "name": "It Never Rains in Southern California",
-            "artist": "Albert Hammond",
-            "artistUrl": "https://open.spotify.com/artist/34E3csCxpXunPGEkOVVX2g",
-            "previewUrl":"https://open.spotify.com/album/0gdQF4mVBPjv5hhjtoe3hM"
-        }],
-
-        "Comments":[{
-            "User_id": "7b7997a2-c0d2-4f8c-b27a-6a1d4b5b8124",
-            "User_name": "Tianxin Liu",
-            "Comment": "Good music!"
-        }]
-    }
-    let listinfo2 = {
-        "Name":"Watch Later",
-        "Type":"movies",
-        "Owner":"Sunii Kim",
-        "Status":"public",
-        "Media":[{
-            "poster":"https://m.media-amazon.com/images/M/MV5BMWM4NTFhYjctNzUyNi00NGMwLTk3NTYtMDIyNTZmMzRlYmQyXkEyXkFqcGdeQXVyMTAwMzUyOTc@._V1_SX300.jpg",
-            "title":"The Sixth Sense",
-            "year":"1999",
-            "director":"M. Night Shyamalan",
-            "rating":"8.1",
-            "actors":"Bruce Willis, Haley Joel Osment, Toni Collette, Olivia Williams"}]
-    }
+    let listinfo1 ={"Name":"Favorites","Type":"music","Owner":"JStevens@gmail.com","Status":"public","Media":[{"name":"Judas","artist":"Lady Gaga","artistUrl":"https://open.spotify.com/artist/1HY2Jd0NmPuamShAr6KMms","previewUrl":""},{"name":"Fantasy - Single Version","artist":"Earth, Wind & Fire","artistUrl":"https://open.spotify.com/artist/4QQgXkCYTt3BlENzhyNETg","previewUrl":"https://p.scdn.co/mp3-preview/748c956b57223b2fa392494c7780be4918cfac48?cid=e88b693ca0154aada66f12b2499f8b65"}]}
+    let listinfo2 = {   "Name":"Watch Later","Type":"movies","Owner":"JStevens@gmail.com","Status":"public","Media":[{"poster":"https://m.media-amazon.com/images/M/MV5BYWIwNWMwNjAtZGU1OS00MDQxLWI5YjgtMzBhODY3YzIzM2Q0XkEyXkFqcGdeQXVyMjExMjk0ODk@._V1_SX300.jpg","title":"CSI: Immortality","year":"2015","director":"Louis Shaw Milito, Louis Shaw Milito","rating":"7.6","actors":"Ted Danson, Jorja Fox, Eric Szmanda, Robert David Hall"}]}
 
     const playlist1 = await playlists.addPlayList(listinfo1);
     const playlist2 = await playlists.addPlayList(listinfo2);
 
     await movies.addMovies(movieInfo)
     
-    let userinfo ={
-        "FirstName": "John",
-        "LastName": "Doe",
-        "Email": "JDoe@gmail.com",
-        "Gender": "Male",
-        "Location": "Hoboken",
-        "AboutMe": "description",
-        "Age": "25",
-        "HashedPassword": bcrypt.hashSync("123456",salt).toString(), //Note: save hashed Password here
-        "FavoriteMusicGenres": ["party","country"],
-        "FavoriteMovieGenres": ["drama","romantic"],
-        "Favorites": playlist1._id,
-        "MusicLists": [],
-        "MovieLists": [],
-        "WatchLater": playlist2._id
-    }
+    let userinfo ={"FirstName":"John",
+    "LastName":"Stevens",
+    "Email":"JStevens@gmail.com",
+    "Gender":"Male",
+    "Location":"West New York",
+    "Age":"33",
+    "HashedPassword":"$2a$08$PbNEbd.ulF4qBvfb7H6ed.8h8zFuS.S8Y4WMpo/Ul93Qn7WlP/Gcm",
+    "FavoriteMusicGenres":["disco","electronic"],
+    "FavoriteMovieGenres":["crime","romantic"],
+    "Favorites": playlist1._id,
+    "MusicLists":[],"MovieLists":[],
+    "WatchLater":playlist2._id
+}
     const user = await users.addUser(userinfo);
     console.log(user);
     console.log(await playlists.getPlaylistByObjectId(user.Favorites))
